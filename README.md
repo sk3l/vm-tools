401a18bac930445a7c61378538990f1cb897b156
Michael Skelton <mskelton@panther.skelton.home>
2019-02-17 11:36:25 -0500

    Add packages, make <pool> parameter to setup script.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # vm-tools
 
 Wrappers around libvirt for automation of KVM virtual machines
+
+## Notes
+* current kickstart file is for Centos7
+* kickstart file expects use of _full_ Centos DVD (not minimal)
--- guest-setup.sh
+++ guest-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USAGE="usage : guest-setup.sh <name> --img <path> --disk <path>\
+USAGE="usage : guest-setup.sh <name> --img <path> --pool <pool> --disk <path>\
  [--vol <size>] [--vcpu <cpu_cnt>] [--ram <size>] "
 
 if [[ $# -lt 5 ]] || [[ $# -gt 11 ]]; then
@@ -13,6 +13,7 @@ KS_FILE="$(readlink -qf $(dirname $0))/guest-ks.cfg"
 
 GUEST_NAME=$1
 IMG_PATH=""
+POOL_NAME=""
 DISK_PATH=""
 VOL_SIZE=15
 VCPU_CNT=2
@@ -24,7 +25,10 @@ while [[ $IDX -lt $# ]]; do
    if [[ ${ARGS[$IDX]} == "--img" ]]; then
       ((IDX++))
       IMG_PATH=${ARGS[$IDX]}
-   elif [[ ${ARGS[$IDX]} == "--disk" ]]; then
+   elif [[ ${ARGS[$IDX]} == "--pool" ]]; then
+      ((IDX++))
+      POOL_NAME=${ARGS[$IDX]}
+    elif [[ ${ARGS[$IDX]} == "--disk" ]]; then
       ((IDX++))
       DISK_PATH=${ARGS[$IDX]}
    elif [[ ${ARGS[$IDX]} == "--vol" ]]; then
@@ -55,10 +59,10 @@ if [[ "$DISK_PATH" == "" ]]; then
       exit 2
 fi
 
-$(virsh vol-info --pool images $GUEST_NAME.qcow2 > /dev/null 2>&1)
+$(virsh vol-info --pool $POOL_NAME $GUEST_NAME.qcow2 > /dev/null 2>&1)
 if [[ $?  -ne 0 ]]; then
    echo "Creating volume $GUEST_NAME.qcow2"
-   virsh vol-create-as images $GUEST_NAME.qcow2 "$VOL_SIZE"G --format qcow2
+   virsh vol-create-as $POOL_NAME $GUEST_NAME.qcow2 "$VOL_SIZE"G --format qcow2
 fi
 
    #--location /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1611.iso \
